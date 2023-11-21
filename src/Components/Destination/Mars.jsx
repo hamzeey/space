39eec2bb593fdf527761e1d341bd0d4072d58812
Hamza Habibullah <Habibullahamza@gmail.com>
2023-11-21 19:13:33 +0100

    destination section completed for mobile

--- src/Components/Destination/Mars.jsx
+++ src/Components/Destination/Mars.jsx
@@ -1,8 +1,25 @@
-
+// import mars from '../../Asssets/image-mars.png'
 
 function Mars() {
     return (
-        <div>Mars</div>
+        <div>
+            <div>
+                <h2 className="flex justify-center mt-3 font-bellefair text-4xl">MARS</h2>
+                <p className="text-center mx-6 mt-2 text-gray-300">Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>
+            </div>
+            <hr className="border-b-2 border-gray-800 mt-3 mx-10" />
+
+            <div className="text-center">
+                <div className="mt-2">
+                    <h2 className="font-barlow text-base text-gray-300">AVG. DISTANCE</h2>
+                    <h3 className="mt-1 font-bellefair text-2xl">225 mil. km</h3>
+                </div>
+                <div className="mt-2">
+                    <h2 className="font-barlow text-base text-gray-300">EST. TRAVEL TIME</h2>
+                    <h3 className="mt-1 font-bellefair text-2xl ">9 months</h3>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Destination/Moon.jsx
+++ src/Components/Destination/Moon.jsx
@@ -1,10 +1,23 @@
-// import moonimg from '../../Asssets/image-moon.png'
+
 
 function Moon() {
     return (
-        <div >
-            <div className='flex justify-center'>
-                {/* <img src={moonimg} className='w-48' /> */}
+        <div>
+            <div>
+                <h2 className="flex justify-center mt-3 font-bellefair text-4xl">MOON</h2>
+                <p className="text-center mx-6 mt-2 text-gray-300">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+            </div>
+            <hr className="border-b-2 border-gray-800 mt-3 mx-10" />
+
+            <div className="text-center">
+                <div className="mt-2">
+                    <h2 className="font-barlow text-base text-gray-300">AVG. DISTANCE</h2>
+                    <h3 className="mt-1 font-bellefair text-2xl">384,400 km</h3>
+                </div>
+                <div className="mt-2">
+                    <h2 className="font-barlow text-base text-gray-300">EST. TRAVEL TIME</h2>
+                    <h3 className="mt-1 font-bellefair text-2xl ">3 days</h3>
+                </div>
             </div>
         </div>
     )

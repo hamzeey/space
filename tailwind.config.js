2985eb0b159d6f2d90058a0fdc9d9c9b76635e4d
Hamza Habibullah <Habibullahamza@gmail.com>
2023-11-27 17:13:49 +0100

    bug fixes done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,18 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'home-mobile': "url('./src/Asssets/background-home-mobile.jpg')",
-        'home-tablet': "url('./src/Asssets/background-home-tablet.jpg')",
-        'home-desktop': "url('./src/Asssets/background-home-desktop.jpg')",
-        'destination-mobile': "url('./src/Asssets/background-destination-mobile.jpg')",
-        'destination-tablet': "url('./src/Asssets/background-destination-tablet.jpg')",
-        'destination-desktop': "url('./src/Asssets/background-destination-desktop.jpg')",
-        'crew-mobile': "url('./src/Asssets/background-crew-mobile.jpg')",
-        'crew-tablet': "url('./src/Asssets/background-crew-tablet.jpg')",
-        'crew-desktop': "url('./src/Asssets/background-crew-desktop.jpg')",
-        'technology-mobile': "url('./src/Asssets/background-technology-mobile.jpg')",
-        'technology-tablet': "url('./src/Asssets/background-technology-tablet.jpg')",
-        'technology-desktop': "url('./src/Asssets/background-technology-desktop.jpg')",
+        'home-mobile': "url('/src/Asssets/background-home-mobile.jpg')",
+        'home-tablet': "url('/src/Asssets/background-home-tablet.jpg')",
+        'home-desktop': "url('/src/Asssets/background-home-desktop.jpg')",
+        'destination-mobile': "url('/src/Asssets/background-destination-mobile.jpg')",
+        'destination-tablet': "url('/src/Asssets/background-destination-tablet.jpg')",
+        'destination-desktop': "url('/src/Asssets/background-destination-desktop.jpg')",
+        'crew-mobile': "url('/src/Asssets/background-crew-mobile.jpg')",
+        'crew-tablet': "url('/src/Asssets/background-crew-tablet.jpg')",
+        'crew-desktop': "url('/src/Asssets/background-crew-desktop.jpg')",
+        'technology-mobile': "url('/src/Asssets/background-technology-mobile.jpg')",
+        'technology-tablet': "url('/src/Asssets/background-technology-tablet.jpg')",
+        'technology-desktop': "url('/src/Asssets/background-technology-desktop.jpg')",
 
       },
 
